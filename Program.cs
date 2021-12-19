96b194a6a7c9811d169364859bff9edcdd7dfb33
Asier <66691307+ASX21@users.noreply.github.com>
2021-12-19 18:27:51 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DiscordCSBot
+{
+    class Program
+    {
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+        public async Task MainAsync()
+        {
+
+        }
+    }
+}
