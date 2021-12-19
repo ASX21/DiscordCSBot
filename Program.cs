1ffadbb5a3907a54d6794f8fb47a805833ad6376
Asier <66691307+ASX21@users.noreply.github.com>
2021-12-19 19:40:56 +0100

    Added LoggingService Test Class and a MessageUpdated test method

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.IO;
+using Discord.WebSocket;
+using Discord;
 
 namespace DiscordCSBot
 {
     class Program
     {
+        private DiscordSocketClient _client;
+
         public static Task Main(string[] args) => new Program().MainAsync();
 
         public async Task MainAsync()
@@ -20,7 +24,29 @@ namespace DiscordCSBot
                 Console.WriteLine("Error reading the file \"APIKey.txt\"");
                 Console.WriteLine(e.Message);
             }
-            Console.WriteLine(token);
+
+            _client = new DiscordSocketClient(
+                new DiscordSocketConfig { MessageCacheSize = 100 });
+            _client.Log += Log;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            _client.MessageUpdated += _client_MessageUpdated;
+
+            await Task.Delay(-1);
+        }
+
+        private async Task _client_MessageUpdated(Cacheable<IMessage, ulong> arg1, SocketMessage arg2, ISocketMessageChannel arg3)
+        {
+            var message = await arg1.DownloadAsync();
+            Console.WriteLine($"{message} => {arg2} in {arg3.Name}");
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
